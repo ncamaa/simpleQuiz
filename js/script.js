4958fbb1d929469fd3686d0a378ea10186b43480
ncamaa <ncamaa@gmail.com>
2017-03-18 21:58:02 +0200

    this is my original code

---
+++ js/script.js
@@ -0,0 +1,41 @@
+function submitAnswers()
+{
+  var total = 5;
+  var score = 0;
+  //Get User input
+  //alert(total);
+  //alert("check");
+  var q1 = document.forms["quizForm"]["q1"].value;
+  var q2 = document.forms["quizForm"]["q2"].value;
+  var q3 = document.forms["quizForm"]["q3"].value;
+  var q4 = document.forms["quizForm"]["q4"].value;
+  var q5 = document.forms["quizForm"]["q5"].value;
+
+  //validation
+  for(i = 1; i <= total; i++)
+  {
+    if(eval('q'+i) == null || eval('q'+i) == '')
+    {
+      alert("you missed question No. " + i + ", please choose an answer");
+      return false;
+    }
+  }
+  //determine the correct answers
+  var answers = ["b", "c", "a", "b", "a"];
+
+  //check answers
+  for (j = 1; j <= total; j++)
+  {
+    if (eval('q'+j) == answers[j-1])
+      score++;
+  }
+
+  //display resolts
+  var results = document.getElementById('results');
+  results.innerHTML = '<h3> You scored <span> '+score+' </span> out of <span>'+total+'</span></h3> ';
+  alert("You scored " + score + " out of " + total);
+
+
+
+  return false;
+}
